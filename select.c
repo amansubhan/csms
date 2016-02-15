cfeb5dc76a8d0fed383992f5440aef28d492402b
Aman Subhan <aman@yunustextile.com>
2016-02-15 16:40:50 +0500

    Update

--- select.c
+++ select.c
@@ -3,10 +3,6 @@
  * Functions Declarations:
  */
 void err_handler(OCI_Error *err);
-void *createDBconnection();
-int executeDBquery(char *sql);
-int closeDBconnection();
-OCI_Connection* cn;
 
 /**
  * Main program
@@ -16,46 +12,26 @@ OCI_Connection* cn;
  */
 int main(int argc, char *argv[])
 {
-    createDBconnection();
-    executeDBquery("select id,num,msg from sms");
-    closeDBconnection();
-}
-    
-/**
- * @return
- */
-void *createDBconnection() {
     OCI_Connection* cn;
+    OCI_Statement* st;
+    OCI_Resultset* rs;
+
     if (!OCI_Initialize(err_handler, NULL, OCI_ENV_DEFAULT))
-        return (void *)EXIT_FAILURE;
-    cn = OCI_ConnectionCreate("ytmdb3.ytm.net:1521/ora9", "hr2000", "scinide007", OCI_SESSION_DEFAULT);
+        return EXIT_FAILURE;
+    cn = OCI_ConnectionCreate("ytmdb2.ytm.net:1521/oracle91", "scott", "tiger", OCI_SESSION_DEFAULT);
 
     printf("Server major    version : %i\n",   OCI_GetServerMajorVersion(cn));
     printf("Server minor    version : %i\n",   OCI_GetServerMinorVersion(cn));
     printf("Server revision version : %i\n\n", OCI_GetServerRevisionVersion(cn));
     printf("Connection      version : %i\n\n", OCI_GetVersionConnection(cn)); 
-    return (void *)cn;
-}
 
-/**
- * @param
- * @return
- */
-int executeDBquery(char *sql) {
-    OCI_Statement* st;
-    OCI_Resultset* rs;
+    char sql[100] = "select empno, ename, job from emp";
     st = OCI_StatementCreate(cn);
     OCI_ExecuteStmt(st, sql);
     rs = OCI_GetResultset(st);
     while (OCI_FetchNext(rs))    {
         printf("%i - %s - %s \n", OCI_GetInt(rs, 1), OCI_GetString(rs,2), OCI_GetString(rs,3));
     }
-}
-
-/**
- * @return
- */
-int closeDBconnection() {
     OCI_Cleanup();
     return EXIT_SUCCESS;   
 }
